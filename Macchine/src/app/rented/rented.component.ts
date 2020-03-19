876b4ab6eaf2106e74ffef3e59836db09f643f2f
FioritoGiovanni <fiorito.giovanni@iisgalvanimi.edu.it>
2020-03-19 11:31:36 +0000

    ultima speranza

---
+++ Macchine/src/app/rented/rented.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Driving } from '../driving.model';
+
+@Component({
+  selector: 'app-rented',
+  templateUrl: './rented.component.html',
+  styleUrls: ['./rented.component.css']
+})
+export class RentedComponent implements OnInit {
+
+    @Input() listaDriving : Driving[]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
